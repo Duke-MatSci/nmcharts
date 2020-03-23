a65825fb51364a5ecd537bea079f3649bdf2ad86
mdeagen <43749866+mdeagen@users.noreply.github.com>
2020-03-23 16:15:50 -0400

    added VL spec, thumbnail

--- UploadingSchema/schema-template.js
+++ UploadingSchema/schema-template.js
@@ -4,19 +4,44 @@
 // => +Replace the data property with the following
 //     "data": {"values": this.data},
 
-class SchemaName {
+class MD20200323g {
     constructor(args) {
         this.data = args;
         this.events();
     }
 
     events(){
-        const returnSchema = { 
-            // Remove Opening and Closing Curly Braces & Paste Your Vega Generated Schema Here
+        const returnSchema = {
+          "$schema": "https://vega.github.io/schema/vega-lite/v4.json",
+          "description": "Example plot of NanoMine data.",
+          "data": {"values": this.data},
+          "autosize":"fit",
+          "transform": [
+              {
+                  "aggregate": [{
+                      "op": "mean",
+                      "field": "Tg",
+                      "as": "mean_Tg"
+                  }],
+                  "groupby": ["Matrix"]
+              }
+          ],
+          "width":360,
+          "height":360,
+          "mark":"bar",
+          "encoding": {
+            "y": {"field":"mean_Tg","type": "quantitative","axis":{"title":"Mean Tg (Celsius)"}},
+            "x": {"field":"Matrix","type": "nominal"},        
+            "tooltip": {"field": "Matrix", "type": "nominal"}
+          },
+          "usermeta": {
+            "title": "Aggregated Mean Tg Grouped by Matrix",
+            "description": "Mean glass transition temperature, aggregated and calculated within Vega-Lite. These data are grouped by matrix type."
+          }
         }
 
         return returnSchema; 
     }
 }
 
-export default SchemaName;
+export default MD20200323g;
