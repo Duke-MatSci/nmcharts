d952904b3c1b30f1e51274b0c84482ea97f00d4a
mdeagen <43749866+mdeagen@users.noreply.github.com>
2020-03-23 15:07:46 -0400

    Ning et al dielectric spectroscopy
    added "autosize" and "usermeta", uploaded dataset

--- UploadingSchema/schema-template.js
+++ UploadingSchema/schema-template.js
@@ -14,124 +14,47 @@ class MD20200323a {
         const returnSchema = {
           "$schema": "https://vega.github.io/schema/vega-lite/v4.json",
           "description": "Example plot of NanoMine data.",
-          "data": {"values": this.data},
+          "data": {"url": "Ning_dielectric.csv"},
           "autosize":"fit",
-          "transform": [       
-              {
-                "filter": "datum.Conductivity > 0"
-              },
-              {
-                "filter": "datum.Filler != null"
-              },
-              {
-                "calculate": "log(datum.Conductivity)/log(10)", "as": "logCond"
-              },
-              {
-                "bin":{"step": 1},
-                "field": "logCond",
-                "as": "bin_logCond"
-              },
-              {
-                "calculate": "pow(10, datum.bin_logCond)", "as": "x1"
-              }, 
-              {
-                "calculate": "pow(10, datum.bin_logCond_end)", "as": "x2"
-              }
-          ],
-          "vconcat": [{
-            "width":360,
-            "height":270,
-            "selection": {
-              "selectfillermatls":{
-                "type": "multi", "fields": ["Filler"], "bind":"legend"
-              },
-              "mybrush": {
-                "type": "interval", "encodings": ["x"]
+          "hconcat":[
+            {
+              "transform": [
+                {"filter":{"selection":"freqselect"}}
+              ],
+              "height":300,
+              "width":300,
+              "mark": {"type": "point", "opacity": 0.4, "filled": true, "tooltip":true},
+              "encoding": {
+                "x": {"field": "e_real", "type": "quantitative", "scale":{"domain":[2.2,3]},
+                       "axis":{"title":"Real Permittivity","titleFontSize": 20,
+                         "labelFontSize":14}},
+                "y": {"field": "e_loss", "type": "quantitative", "scale":{"domain":[0,0.135]},
+                       "axis":{"title":"Loss Permittivity","titleFontSize": 20,
+                         "labelFontSize":14}},
+                "shape": {"field": "Matrix Mw", "type": "ordinal","scale":{"domain":["4k","152k"]}},
+                "color": {"field": "Loading", "type": "nominal"},
+                "size":{"field":"Treatment", "type": "nominal","scale":{"range":[100,250]}}
               }
             },
-            "mark":"bar",
-            "encoding": {
-              "x":{
-                "field":"x1",
-                "type": "quantitative",
-                "scale": {"type": "log"},
-                "axis":{"tickCount": 5, "title": "Electrical Conductivity (S/m)",
-                    "titleFontSize": 16, "labelFontSize":16}            
+            {
+              "height":40,
+              "width":200,
+              "mark":"tick",
+              "selection": {
+                "freqselect": {"type": "interval", "encodings": ["x"]}
               },
-              "x2":{
-                  "field": "x2"
-              },
-              "y":{
-                "aggregate": "count",
-                "type": "quantitative",
-                "axis":{"title":"Number of Samples","titleFontSize": 16,"labelFontSize":16}
-              },
-              "color":{
-                "field":"Filler",
-                "type": "nominal",
-                "legend":{"titleFontSize": 16, "title":"Filler Type"}
-              },
-              "opacity":{
-                "condition":{"selection":"selectfillermatls","value": 0.95},
-                "value": 0.05
+              "encoding": {
+                "x":{"field":"Freq", "type": "quantitative", "scale":{"type": "log"},
+                      "axis":{"title":"Frequency (Hz)", "titleFontSize": 20,
+                        "labelFontSize":14,
+                        "labelAngle": 90}}
               }
             }
-          },
-          {
-            "width":360,
-            "height":220,
-            "encoding": {
-              "x":{
-                "field":"Conductivity",
-                "type": "quantitative",
-                "scale": {"type": "log", "domain":{"selection": "mybrush"}},
-                "axis":{"tickCount": 5, "title": "Electrical Conductivity (S/m)",
-                    "titleFontSize": 16, "labelFontSize":16}           
-              },
-              "color":{
-                "field":"Filler",
-                "type": "nominal",
-                "legend":{"titleFontSize": 16, "title":"Filler Type"}
-              },
-              "opacity":{
-                "condition":{
-                  "selection":"selectfillermatls","value": 0.8
-                },
-                "value": 0.01
-              }
-            },
-            "layer": [
-              {
-                "mark": {"type": "point", "opacity": 0.8, "size": 300, 
-                  "filled": true, "clip": true, "shape":"triangle-left"},
-                "encoding": {
-                  "y": {"field": "Loading_mass", "type": "quantitative", 
-                    "scale": {"type":"log"},"axis":{
-                      "title":"Loading (mass fraction)",
-                      "titleFontSize": 16,
-                      "labelFontSize": 16
-                    }}
-                }
-              },
-              {
-                "mark": {"type": "point", "opacity": 0.8, "size": 300, 
-                  "filled": true, "clip": true, "shape":"triangle-right"},
-                "encoding": {
-                  "y": {"field": "Loading_vol", "type": "quantitative", 
-                    "scale": {"type":"log"},"axis":{
-                      "title":"Loading (volume fraction)",
-                      "titleFontSize": 16,
-                      "labelFontSize": 16
-                    }}
-                }
-              }
-            ],
-            "resolve": {"scale": {"y":"independent"}}
-          }],
+          ],
           "usermeta": {
-            "title": "Electrical Conductivity of Polymer Nanocomposites",
-            "description": "In the legend, click or SHIFT+click to highlight specific filler materials. In the bar chart, drag to define a selection brush and adjust the horizontal scale of the scatter plot below. Click and drag the selection brush left or right to view different conductivity ranges."
-          }  
+            "title": "Dielectric Spectroscopy Data Reimagined",
+            "description": "Click-and-drag along the Frequency tick plot to define a frequency range. Drag the selection to sweep across different frequency ranges and watch the scatter plot adjust in real-time. Data in this chart were provided by Ning et al. (DOI: 10.1021/acsmacrolett.9b00619), who studied silica-filled polyethylene at various matrix molecular weight and heat treatment."
+          }
         }
 
         return returnSchema; 
