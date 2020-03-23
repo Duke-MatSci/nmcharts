5d9857875eff9ec9d26b658d7832dd586b818f8e
Tolulomo Fateye <tolulomofateye@tolulomos-mbp.home>
2020-03-23 13:09:34 -0400

    Schema Upload Folder Template

---
+++ UploadingSchema/schema-template.js
@@ -0,0 +1,22 @@
+// Note:
+// => +Provide a class name for the schema,
+// => +Copy and Paste your schema into the returnSchema curly brace
+// => +Replace the data property with the following
+//     "data": {"values": this.data},
+
+class SchemaName {
+    constructor(args) {
+        this.data = args;
+        this.events();
+    }
+
+    events(){
+        const returnSchema = { 
+            // Remove Opening and Closing Curly Braces & Paste Your Vega Generated Schema Here
+        }
+
+        return returnSchema; 
+    }
+}
+
+export default SchemaName;
