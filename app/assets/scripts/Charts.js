95c9c7f9829dbc451e23f279647c5864af6d4d11
Tolulomo Fateye <tolulomofateye@tolulomos-mbp.home>
2020-03-24 03:07:46 -0400

    Uploading schema dir

--- app/assets/scripts/Charts.js
+++ app/assets/scripts/Charts.js
@@ -5,22 +5,6 @@ import Display from './misc/display';
 
 new Display();
 
-// fetch('assets/files/nano.json', {mode: 'no-cors'})
-//     .then(function(result){
-//         return result.text()
-//     }).then(function(data){
-//         const vsc = new VSchema(JSON.parse(data))
-//         return vsc.events()
-//     }).then(function(processedSchema){
-//         if(isMobile){
-//             vegaEmbed('#vis', processedSchema);
-//             vegaEmbed('#vis2', processedSchema);
-//             vegaEmbed('#vis3', processedSchema);
-//             return vegaEmbed('#vis4', processedSchema);
-//         }
-//         return vegaEmbed('#vis', processedSchema);
-//     })
-
 
 if(module.hot){
     module.hot.accept();
